c9e3001b0006bfa857ef1ed080364c6ef295879c
Yaroslav Borovyi <borovyi0808@gmail.com>
2020-08-30 21:54:39 +0300

    int course project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { DatabidingPaticeComponent } from './databiding-patice/databiding-patice.component';
-import { DirectivePracticeComponent } from './directive-practice/directive-practice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent,
-    DatabidingPaticeComponent,
-    DirectivePracticeComponent
   ],
   imports: [
     BrowserModule,
