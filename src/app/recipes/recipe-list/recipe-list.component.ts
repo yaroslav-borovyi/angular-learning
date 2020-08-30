b0dfe08f1da37afd3264e679716bc89468284597
Yaroslav Borovyi <borovyi0808@gmail.com>
2020-08-30 22:51:26 +0300

    add recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Pizza', 'Super cheese pizza!', 'https://i.pinimg.com/originals/5b/dd/07/5bdd070ea344c21ef870c8d81dee345e.jpg'),
+  ];
 
   constructor() { }
 
@@ -13,3 +18,4 @@ export class RecipeListComponent implements OnInit {
   }
 
 }
+ 
